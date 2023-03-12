bc2702144ae8f97fd8e525f6b90fd85b55db464f
lanbiltrading <127685735+lanbiltrading@users.noreply.github.com>
2023-03-12 14:13:11 -0500

    Update CronJob.py

--- CronJob.py
+++ CronJob.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/run-colab')
 def run_colab():
-    gdown.download('https://colab.research.google.com/drive/1eM-qcbRigk1ISGyaXf9lObcGjiUzoHAa?usp=sharing', 'colab.ipynb', quiet=False)
+    gdown.download('https://colab.research.google.com/drive/1eM-qcbRigk1ISGyaXf9lObcGjiUzoHAa?usp=sharing', 'tradingBot.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
